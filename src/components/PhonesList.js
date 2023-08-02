a6f4022b9c65e7c067e6f67c1c944f0f15253513
Lucas Silva <gaviao.lucas@gmail.com>
2023-08-02 19:34:44 +0100

    functionality working

--- src/components/PhonesList.js
+++ src/components/PhonesList.js
@@ -6,6 +6,7 @@ function PhonesList() {
 
   const [phonesList, setPhones] = useState([]);
   const [onePhone, setOnePhone] = useState({});
+  const [display, setDisplay] = useState(false);
 
   const getPhones = () => {
     axios
@@ -24,6 +25,7 @@ function PhonesList() {
       .get(`${apiUrl}/phones/${phoneId}`)
       .then((response) => {
         setOnePhone(response.data);
+        setDisplay(true);
         console.log(onePhone);
       })
       .catch((err) => console.log("error to get one detail", err));
@@ -31,29 +33,49 @@ function PhonesList() {
 
   useEffect(() => {
     getPhones();
+    // eslint-disable-next-line
   }, []);
 
   return (
-    <div>
-      {phonesList.map((element, i) => (
-        <ul key={i}>
-          <li>
-            <button
-              onClick={(e) => {
-                handleClick(element._id, e);
-              }}
-            >
-              {element.name}
-            </button>
-          </li>
-          <li>{element._id}</li>
-        </ul>
-      ))}
-      <>
-        <ul>
-          <li>{onePhone.manufacturer}</li>
-        </ul>
-      </>
+    <div className="MainDiv">
+      <div className="ButtonEl">
+        {phonesList.map((element, i) => (
+          <ul key={i}>
+            <li>
+              <button
+                onClick={(e) => {
+                  handleClick(element._id, e);
+                }}
+              >
+                {element.name}
+              </button>
+            </li>
+          </ul>
+        ))}
+      </div>
+      {display ? (
+        <div className="divInfo">
+          <img src={onePhone.imageFileName} alt="phonePicture" />
+          <h3>{onePhone.name}</h3>
+          <h4>
+            <b>Manufacturer:</b> {onePhone.manufacturer}
+          </h4>
+          <p>
+            <b>Description:</b> {onePhone.description}
+          </p>
+          <p>
+            <b>Price:</b> {onePhone.price}
+          </p>
+          <p>
+            <b>Screen:</b> {onePhone.screen}
+          </p>
+          <p>
+            <b>Ram:</b> {onePhone.ram}
+          </p>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
